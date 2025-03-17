0f6fbd92cbc98e1c485015df988746b994c70000
sreelakhmi-xspine <sreelakshmi@xspine.in>
2025-03-17 16:28:09 +0530

    library system project added

---
+++ src/sreelakshmi-trainer/library_management_system/Library.java
@@ -0,0 +1,21 @@
+package library_management_system;
+
+import java.util.ArrayList;
+
+public class Library {
+    public static void main(String[] args) {
+        // ArrayList to store books
+        ArrayList<Book> library = new ArrayList<>();
+
+        // Adding books to the library
+        library.add(new Book("The Alchemist", "Paulo Coelho", 15.99));
+        library.add(new Book("Atomic Habits", "James Clear", 12.50));
+        library.add(new Book("Rich Dad Poor Dad", "Robert Kiyosaki", 18.75));
+
+        // Displaying all books
+        System.out.println("Library Book List:");
+        for (Book book : library) {
+            book.displayBook();
+        }
+    }
+}
