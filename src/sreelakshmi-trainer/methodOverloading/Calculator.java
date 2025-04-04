222e3037387412e43985cfdfac6e407b89ef6fdd
sreelakhmi-xspine <sreelakshmi@xspine.in>
2025-04-04 12:19:46 +0530

    method overloading added

---
+++ src/sreelakshmi-trainer/methodOverloading/Calculator.java
@@ -0,0 +1,18 @@
+package methodOverloading;
+
+public class Calculator {
+    // Method to add two integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to add three integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Method to add two double values
+    double add(double a, double b) {
+        return a + b;
+    }
+}
