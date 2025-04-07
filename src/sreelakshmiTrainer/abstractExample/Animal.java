54465324a3369c96f97edb40d917605f6812f977
sreelakhmi-xspine <sreelakshmi@xspine.in>
2025-04-07 14:31:42 +0530

    abstract class updated

--- src/sreelakshmiTrainer/abstractExample/Animal.java
+++ src/sreelakshmiTrainer/abstractExample/Animal.java
@@ -11,7 +11,6 @@ abstract class Animal {
 // Subclass (inherit from Animal)
 class Pig extends Animal {
     public void animalSound() {
-        // The body of animalSound() is provided here
         System.out.println("The pig says: wee wee");
     }
 }
