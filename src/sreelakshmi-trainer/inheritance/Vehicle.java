32ed328d788eed9f64a3df5fd7ffbc4e22e12942
sreelakhmi-xspine <sreelakshmi@xspine.in>
2025-04-07 14:32:51 +0530

    abstract class updated

--- src/sreelakshmi-trainer/inheritance/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package inheritance;
-
-public class Vehicle {
-    public String brand = "Ford";
-
-    public void brandNaming() {
-        System.out.println("The brand name is " + brand);
-    }
-}
